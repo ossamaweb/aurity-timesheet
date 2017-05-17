ee71680e0de3dee9288b70e105ca4352873cf431
ossama <getroundapp@gmail.com>
2017-05-17 20:10:46 +0100

    merge api data & calendar data

--- src/components/timesheet/calendar/calendar.js
+++ src/components/timesheet/calendar/calendar.js
@@ -1,9 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { months, days, getDaysPerMonth } from '../../../constants';
-import { range, slice, sum, find, propEq } from 'ramda';
+import { range, slice, sum, find, propEq, forEach, isNil } from 'ramda';
 
 const DAYS_IN_WEEK = 7;
+
+function generateWeeksObjects(data) {
+  const weeks = [];
+  const generateWeeksObjects = ({ week_id, week_number, days_in_week }) => {
+    const daysObjet = [];
+    const generateDaysObjects = (day_number) => {
+      const dayObject = {
+        day_number,
+        id: `${week_number}${day_number}`,
+        hours: null,
+        minutes: null
+      };
+      daysObjet.push(dayObject);
+    };
+
+    forEach(generateDaysObjects, days_in_week);
+    const weekObject = {
+      week_id,
+      week_number,
+      days_in_week: daysObjet
+    };      
+    weeks.push(weekObject);
+  };
+    
+  forEach(generateWeeksObjects, data);
+  return weeks;
+}
 
 function generateCalendar(selectedMonth, selectedYear) {
   const daysPerMonth = getDaysPerMonth(selectedYear);
@@ -33,18 +60,91 @@ function generateCalendar(selectedMonth, selectedYear) {
       {
         week_number: firstWeekNumber + index,
         week_id: index,
-        days: weekDays
+        days_in_week: weekDays
       }
     );
   }
   // rows left at the end
-  const numOfNextMonthDays = 7 - weeks[numOfWeeks-1].days.length;
+  const numOfNextMonthDays = 7 - weeks[numOfWeeks-1].days_in_week.length;
   if (numOfNextMonthDays > 0) {
     // insert next month dates
-    weeks[numOfWeeks-1].days.push(...range(1, numOfNextMonthDays +1));
+    weeks[numOfWeeks-1].days_in_week.push(...range(1, numOfNextMonthDays +1));
   }
 
-  return weeks;
+  return generateWeeksObjects(weeks);
+}
+
+function mergeDays(weekDays, apiWeekDays) {
+  const mergedDays = [];
+  const mergeDayObject = (week) => {
+    const apiDays = find(propEq('day_number', week.day_number))(apiWeekDays);
+    if (apiDays) {
+      mergedDays.push(apiDays);
+    } else {
+      mergedDays.push(week);
+    }
+
+  };
+  forEach(mergeDayObject, weekDays);
+  return mergedDays;
+}
+
+function mergeCalendar(calendarData, apiData) {
+  let mergedData = [];
+  const mergeWeekObject = (week) => {
+    const apiWeek = find(propEq('week_number', week.week_number))(apiData);
+    console.log('apiWeek', apiWeek);
+    if (apiWeek) {
+      const mergedDays =  mergeDays(week.days_in_week, apiWeek.days_in_week);
+      apiWeek.days_in_week = mergedDays;
+      mergedData.push(apiWeek);
+    } else {
+      mergedData.push(week);
+    }
+
+  };
+  // loop through calendar data and merge existing api weeks
+  forEach(mergeWeekObject, calendarData);
+  return mergedData;
+}
+// merge calendar data with api data
+function mergeData(calendarData, apiData) {
+
+  const mergedData = [];
+  const generateWeeksObjects = ({ week_id, week_number, days_in_week }) => {
+    const daysObjet = [];
+    // find week data by week number
+    const apiWeekData = apiData ? find(propEq('week_number', week_number))(apiData) : null;
+    const generateDaysObjects = (day_number) => {
+      // find day data by day number
+      const apiDayData = apiWeekData && apiWeekData.days_in_week ? find(propEq('day_number', day_number))(apiWeekData.days_in_week) : null;
+
+      const dayObject = {
+        day_number,
+        id: `${week_number}${day_number}`,
+      };
+      if (apiDayData) {
+        dayObject.minutes = apiDayData.minutes;
+        dayObject.hours = apiDayData.hours;
+      }
+      daysObjet.push(dayObject);
+    };
+
+    forEach(generateDaysObjects, days_in_week);
+    const weekObject = {
+      week_id,
+      week_number,
+      days_in_week: daysObjet
+    };
+    if (apiWeekData) {
+      weekObject.status = apiWeekData.status;
+    }
+      
+    mergedData.push(weekObject);
+  };
+    
+  forEach(generateWeeksObjects, calendarData);
+  return mergedData;
 }
 
 export default function Calendar({
@@ -57,7 +157,7 @@ export default function Calendar({
   handlePrevMonth
 }) {
 
-  const renderWeek = (weekId, weekNumber, days) => {
+  const renderWeek2 = (weekId, weekNumber, days) => {
     const weekData = find(propEq('week_number', weekNumber))(data);
     return (
       <tr key={weekId} className={weekData ? weekData.status : ''}>
@@ -72,7 +172,11 @@ export default function Calendar({
     );
   }
 
-  const calendarData = generateCalendar(selectedMonth, selectedYear);
+  let calendarData = generateCalendar(selectedMonth, selectedYear);
+  if (data && data.length > 0) {
+    calendarData = mergeCalendar(calendarData, data);
+  }
+  console.log('calendarData', calendarData);
   return (
     <div>
       {loading &&
@@ -90,8 +194,10 @@ export default function Calendar({
               </tr>
             </thead>
             <tbody>
-              {calendarData.map(({ week_id, week_number, days }) =>
-                renderWeek(week_id, week_number, days )
+              {calendarData.map(({ week_id, days_in_week, status }) =>
+                <tr key={week_id} className={status}>
+                  {renderWeek(days_in_week)}
+                </tr>
               )}
             </tbody>
           </table>
@@ -112,6 +218,16 @@ Calendar.propTypes = {
   handlePrevMonth: PropTypes.func.isRequired
 }
 
+function renderWeek(days_in_week) {
+  return days_in_week.map(({ id, day_number, hours, minutes }) =>
+    <td key={id}>
+      {day_number}
+      <br />
+      {!isNil(hours) && !isNil(minutes) &&
+        <p>{`${hours}h:${minutes}min`}</p>
+      }  
+    </td>);
+}
 function Day({ day, daysData }) {
   if (!daysData) {
     return (
